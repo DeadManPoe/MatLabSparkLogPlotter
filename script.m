99d6b0a9db4ae8a55429da45f2bf665220a756a1
giorgiopea <annatar93@gmail.com>
2016-12-21 12:09:46 +0100

    mean calculated

--- script.m
+++ script.m
@@ -1,6 +1,7 @@
 file_regex = 'application_([0-9]+)_([0-9]+)_csv';
 directory = './tmp/7Nodes/q26/12Executors'
 files = dir(directory);
+formatspec = '%s%s'
 batch = [];
 execs = [];
 for i=1:length(files)
@@ -8,6 +9,9 @@ for i=1:length(files)
     batch{length(batch)+1} = files(i).name;
   end
 end
-for i=1:length(files)
-  batch{i}
+for i=1:length(batch)
+  tmp = readtable(strcat(directory,'/',batch{i},'/app_1.csv'),...
+  'Delimiter',',','Format',formatspec);
+  execs{length(execs)+1} = str2num(tmp{2,2}{1})-str2num(tmp{1,2}{1})
 end
+int32(mean(cell2mat(execs)))
